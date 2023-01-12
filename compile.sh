d1c4ccd335b5a685a38fa9091953c4df6a01b92c
MegaStrog <122373844+megastrog@users.noreply.github.com>
2023-01-12 23:05:10 +0000

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
-clang aim.c include/k2c_pooling_layers.c include/k2c_convolution_layers.c include/k2c_helper_functions.c include/k2c_core_layers.c include/k2c_activations.c -I include -Ofast -mfma -lX11 -lm -o aim
+clang aim.c include/k2c_pooling_layers.c include/k2c_convolution_layers.c include/k2c_helper_functions.c include/k2c_core_layers.c include/k2c_activations.c -I include -Ofast -mfma -lxdo -lX11 -lm -o aim
 strip --strip-unneeded aim
 upx --lzma --best aim
 ./aim
